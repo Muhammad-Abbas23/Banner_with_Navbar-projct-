b07cf7fc20cb44c397ae9e7100ac4c090e1a7595
Muhammad-Abbas23 <127502250+Muhammad-Abbas23@users.noreply.github.com>
2023-03-10 16:59:19 +0500

    Banner_with_Navbar(projct)
    Hre is my Banner_with_Navbar projct .

---
+++ custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $(".content").hide(0,function(){
+        console.log("mmm");
+    })
+
+    $(".content").show(1800,function(){
+        console.log("mmm");
+    })
+
+
+
+
+
+
+
+
+});
